0f46d638c6f8b9265b053a56de455c9dbc77ce29
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-30 17:29:22 +0530

    implement AllBooks and AddBooks functions in BookController; handle errors and responses

--- server/controllers/BookController.js
+++ server/controllers/BookController.js
@@ -1,13 +1,29 @@
+import BookModel from "../models/BooksModel"
 
 
 export const AllBooks = async (req, res) => {
     try {
-        const 
+        const Books = await BookModel.find({
+            BookId
+        })
+        return res.status(201).status("fetch all books successfully",Books);
     } catch (e) {
-        
+        console.error("Internal server error")
+        return res.status("500")
     }
 }
 
 export const AddBooks = async (req, res) => {
-    
+    try {
+        const { BookName, BookDescription, BookPrice, BookType } = req.body;
+        const newBook = await BookModel.create({
+            BookName,
+            BookDescription,
+            BookPrice,
+            BookType
+        })
+        return res.status("201").json("Book add successfully",newBook);
+    } catch (e) {
+        return res.status("500").json("Internal server error");        
+    }
 }
