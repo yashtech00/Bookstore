d7ecd8e50ea4ba4857ef9a432d67f4a55a05f044
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-30 10:39:08 +0530

    set up express server with CORS and dotenv configuration

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+
+
+
